5de1642bf40ff481ffdfcf98de24c5c78422c580
Dece <shgck@pistache.land>
2019-11-04 22:48:18 +0100

    Disgusting login to access protected threads
    For some reason I can't keep the session up even though the requests
    session manages the cookies, so for now it just keeps POSTing creds
    page by page. Help!

--- krall.py
+++ krall.py
@@ -1,4 +1,5 @@
 import argparse
+import getpass
 import logging
 import os
 import pathlib
@@ -52,8 +53,11 @@ def process_thread(url, output_dir: pathlib.Path):
     if soup is None:
         return
 
+    soup = check_for_login(url, soup)
+
     if not output_dir.is_dir():
         output_dir.mkdir()
+
     title = soup.find(class_='topic-title').get_text()
     title = superslug.slugify(title)
     thread_dir = output_dir / title
@@ -61,12 +65,14 @@ def process_thread(url, output_dir: pathlib.Path):
     process_page(url, thread_dir, 1, soup=soup)
 
 def process_page(url, thread_dir, page, soup=None):
-    print("Processing {}...".format(url))
+    print('Processing {}...'.format(url))
 
     if soup is None:
         soup = get_page_soup(url)
         if soup is None:
             return
+
+    soup = check_for_login(url, soup)
 
     if not thread_dir.is_dir():
         thread_dir.mkdir()
@@ -89,30 +95,38 @@ def process_page(url, thread_dir, page, soup=None):
         return
 
     next_url_path = next_element.a['href'].lstrip('./')
-    parsed = urlparse(url)
-    next_url = "{}://{}/{}".format(parsed.scheme, parsed.netloc, next_url_path)
+    next_url = get_host(url) + '/' + next_url_path
     process_page(next_url, thread_dir, page + 1)
 
-@retry(wait=wait_fixed(10), stop=stop_after_attempt(5))
+@retry(wait=wait_fixed(10), stop=stop_after_attempt(3))
 def get_page_soup(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        print('Invalid status code {} for page {}'.format(
-            response.status_code, url))
-        return None
-    
+    response = (SESH or requests).get(url)
+    response.raise_for_status()
+
     return BeautifulSoup(response.text, 'html.parser')
+
+def check_for_login(url, soup):
+    """ Check for the login form in soup; log in at URL if found. """
+    login_form = soup.find('form', id='login')
+    if login_form is None:
+        # Nothing to do.
+        return soup
+
+    result = login(url, login_form)
+    if result is None:
+        return soup
+    global SESH
+    SESH, soup = result
+    return soup
 
 def download_urls(urls, output_dir):
     for url in urls:
         download_url(url, output_dir)
 
-@retry(wait=wait_fixed(10), stop=stop_after_attempt(5))
+@retry(wait=wait_fixed(10), stop=stop_after_attempt(3))
 def download_url(url, output_dir):
-    response = requests.get(url)
-    if response.status_code != 200:
-        print('Could not get file @ {}'.format(url))
-        return
+    response = (SESH or requests).get(url)
+    response.raise_for_status()
 
     filename = os.path.basename(urlparse(url).path)
     filepath = output_dir / filename
@@ -120,6 +134,45 @@ def download_url(url, output_dir):
         for chunk in response.iter_content(chunk_size=128):
             fd.write(chunk)
     print('Downloaded {}'.format(filepath))
+
+def get_host(url):
+    """ Return host with scheme from parsed URL. """
+    parsed = urlparse(url)
+    return '{}://{}'.format(parsed.scheme, parsed.netloc)
+
+def login(url, form):
+    login_redirect = form['action'].lstrip('./')
+    login_url = get_host(url) + '/' + login_redirect
+
+    global USERNAME, PASSWORD
+    if USERNAME is None or PASSWORD is None:
+        print('This page requires an account with sufficient access.')
+        print('Enter your credentials (scheme is "{}")'.format(url.split('://')[0]))
+        USERNAME = input('Username: ')
+        PASSWORD = getpass.getpass()
+
+    headers = {'User-Agent': USER_AGENT}
+    payload = {
+        'username': USERNAME,
+        'password': PASSWORD,
+        'redirect': form.find('input', attrs={'name': 'redirect'})['value'],
+        'sid': form.find('input', attrs={'name': 'sid'})['value'],
+        'login': form.find('input', attrs={'name': 'login'})['value'],
+    }
+
+    sesh = requests.Session()
+    response = sesh.post(login_url, headers=headers, data=payload)
+
+    # Always returns 200. Check if the login form is not there anymore to call
+    # it a success.
+    response_soup = BeautifulSoup(response.text, 'html.parser')
+    if response_soup.find('form', id='login') is None:
+        logging.debug('Logged in.')
+        return sesh, response_soup
+
+    logging.error('Could not log in.')
+    return None
+
 
 if __name__ == '__main__':
     main()
